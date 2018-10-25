c812b99795587465e462275ab0f9edf740666482
cartacode <cartacode@outlook.com>
2018-10-25 11:22:51 +0100

    add api intercept function

--- background.js
+++ background.js
@@ -45,3 +45,83 @@ function coreGetApi(url) {
 	
 	});
 }
+
+(function() {
+    const tabStorage = {};
+    const networkFilters = {
+        urls: [
+        	"*://*.video.tidal.com/*"
+        ]
+    };
+
+    chrome.webRequest.onBeforeRequest.addListener((details) => {
+        const { tabId, requestId } = details;
+        if (!tabStorage.hasOwnProperty(tabId)) {
+            return;
+        }
+
+        tabStorage[tabId].requests[requestId] = {
+            requestId: requestId,
+            url: details.url,
+            startTime: details.timeStamp,
+            status: 'pending'
+        };
+        // console.log(tabStorage[tabId].requests[requestId]);
+    }, networkFilters);
+
+    chrome.webRequest.onCompleted.addListener((details) => {
+        const { tabId, requestId } = details;
+        if (!tabStorage.hasOwnProperty(tabId) || !tabStorage[tabId].requests.hasOwnProperty(requestId)) {
+            return;
+        }
+
+        const request = tabStorage[tabId].requests[requestId];
+
+        Object.assign(request, {
+            endTime: details.timeStamp,
+            requestDuration: details.timeStamp - request.startTime,
+            status: 'complete'
+        });
+
+        if (
+        	tabStorage[tabId].requests[details.requestId].url.indexOf('master_all.m3u') > -1
+        	&& tabStorage[tabId].requests[details.requestId].status == "complete"
+        ) {
+        	console.log(tabStorage[tabId].requests[details.requestId]);
+        }
+        
+
+    }, networkFilters);
+
+    chrome.webRequest.onErrorOccurred.addListener((details)=> {
+        const { tabId, requestId } = details;
+        if (!tabStorage.hasOwnProperty(tabId) || !tabStorage[tabId].requests.hasOwnProperty(requestId)) {
+            return;
+        }
+
+        const request = tabStorage[tabId].requests[requestId];
+        Object.assign(request, {
+            endTime: details.timeStamp,           
+            status: 'error',
+        });
+        console.log(tabStorage[tabId].requests[requestId]);
+    }, networkFilters);
+
+    chrome.tabs.onActivated.addListener((tab) => {
+        const tabId = tab ? tab.tabId : chrome.tabs.TAB_ID_NONE;
+        if (!tabStorage.hasOwnProperty(tabId)) {
+            tabStorage[tabId] = {
+                id: tabId,
+                requests: {},
+                registerTime: new Date().getTime()
+            };
+        }
+    });
+    chrome.tabs.onRemoved.addListener((tab) => {
+        const tabId = tab.tabId;
+        if (!tabStorage.hasOwnProperty(tabId)) {
+            return;
+        }
+        tabStorage[tabId] = null;
+    });
+}());
--- popup.js
+++ popup.js
@@ -3,8 +3,6 @@ var port = chrome.runtime.connect({
     name: "tidal-api-intercept"
 });
 
-port.postMessage({ type: "getTime", time: time });
-
 chrome.runtime.onConnect.addListener(function(port) {
   port.onMessage.addListener(function(msg) {
     if (typeof (msg) !== "string") {
@@ -16,9 +14,4 @@ chrome.runtime.onConnect.addListener(function(port) {
 
 // get data from storage
 chrome.storage.sync.get("artist", function(obj) {
-  document.getElementById('artist-name').textContent = obj.artist;
-});
-
-chrome.storage.sync.get("title", function(obj) {
-  document.getElementById('title').textContent = obj.title;
 });
